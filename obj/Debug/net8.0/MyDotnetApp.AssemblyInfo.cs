29941ea76ab5c20d0f2ef39d3f746491bc0395e0
sandeepkaran <https://github.com/sandeepNoida7>
2025-10-01 03:08:01 +0530

    kk

--- obj/Debug/net8.0/MyDotnetApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyDotnetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotnetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35912832d2094c783f9a452204219d096f1ebd4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bacbf81821359c4dca4efbf7a139cf40d61264")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotnetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotnetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
